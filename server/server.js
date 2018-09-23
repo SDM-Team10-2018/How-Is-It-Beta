920d7669b8d9b45b352af724684c97e046462c85
djonathanpaul <jonathandarivemulapaul@gmail.com>
2018-09-23 23:36:21 +1200

    ej attempt

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ module.exports = app;
 // ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "index.html"));
+  res.sendFile(path.join(__dirname, "../client"));
 });
 
 app.listen();
